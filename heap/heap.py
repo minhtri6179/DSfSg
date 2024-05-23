b87958dd649334d33acba068b6e07345cf4bd497
minhtri6179 <minhtri.ngt@gmail.com>
2024-05-23 21:34:42 +0700

    feat: add pop minheap

--- heap/heap.py
+++ heap/heap.py
@@ -14,6 +14,34 @@ class MinHeap:
             self.heap[new_idx // 2] = tmp
             new_idx = new_idx // 2
 
+    def pop(self):
+        if len(self.heap) == 1:
+            return None
+        if len(self.heap) == 2:
+            return self.heap.pop()
+
+        res = self.heap[1]
+        self.heap[1] = self.heap.pop()
+        i = 1
+        while 2 * i < len(self.heap):
+            if (
+                2 * i + 1 < len(self.heap)
+                and self.heap[2 * i + 1] < self.heap[2 * i]
+                and self.heap[i] > self.heap[2 * i + 1]
+            ):
+                tmp = self.heap[i]
+                self.heap[i] = self.heap[2 * i + 1]
+                self.heap[2 * i + 1] = tmp
+                i = 2 * i + 1
+            elif self.heap[i] > self.heap[2 * i]:
+                tmp = self.heap[i]
+                self.heap[i] = self.heap[2 * i]
+                self.heap[2 * i] = tmp
+                i = 2 * i
+            else:
+                break
+        return res
+
 
 min_heap = MinHeap()
 min_heap.push(9)
@@ -21,3 +49,6 @@ min_heap.push(2)
 min_heap.push(7)
 min_heap.push(8)
 min_heap.push(4)
+
+while not min_heap.is_empty():
+    print(min_heap.pop(), end=" ")
