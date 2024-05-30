a93571ae86dbbe12d98117bc50ea4c3032283e49
minhtri6179 <minhtri.ngt@gmail.com>
2024-05-30 17:11:13 +0700

    dp: lcs

---
+++ dynamic-programing/LCS.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/longest-common-subsequence/description/
+# competitiveprograming, pythonprogram
+# -> ptprogram, len() = 9
+text1 = input()
+text2 = input()
+
+
+def longestCommonSubsequence(text1, text2):
+    m, n = len(text1), len(text2)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+            if text1[i - 1] == text2[j - 1]:
+                dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] + 1)
+    return dp[m][n]
+
+
+res = longestCommonSubsequence(text1, text2)
+
+print(res)
