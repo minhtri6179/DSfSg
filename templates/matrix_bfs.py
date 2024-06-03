08420ba0b0d3a510b9de2ebbe117ad29e876b27d
minhtri6179 <minhtri.ngt@gmail.com>
2024-06-03 21:42:22 +0700

    matrix: swap

--- templates/matrix_bfs.py
+++ templates/matrix_bfs.py
@@ -1,22 +1,31 @@
-# template from neetcode
-grid = [[0, 0, 0, 0], [1, 1, 0, 0], [0, 0, 0, 1], [0, 1, 0, 0]]
+# BFS template from neetcode.io
+from collections import deque
 
 
-# Count paths (backtracking)
-def dfs(grid, r, c, visit):
+def bfs(grid):
     ROWS, COLS = len(grid), len(grid[0])
-    if min(r, c) < 0 or r == ROWS or c == COLS or (r, c) in visit or grid[r][c] == 1:
-        return 0
-    if r == ROWS - 1 and c == COLS - 1:
-        return 1
+    visit = set()
+    queue = deque()
+    queue.append((0, 0))
+    visit.add((0, 0))
 
-    visit.add((r, c))
+    length = 0
+    while queue:
+        for i in range(len(queue)):
+            r, c = queue.popleft()
+            if r == ROWS - 1 and c == COLS - 1:
+                return length
 
-    count = 0
-    count += dfs(grid, r + 1, c, visit)
-    count += dfs(grid, r - 1, c, visit)
-    count += dfs(grid, r, c + 1, visit)
-    count += dfs(grid, r, c - 1, visit)
-
-    visit.remove((r, c))
-    return count
+            neighbors = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+            for dr, dc in neighbors:
+                if (
+                    min(r + dr, c + dc) < 0
+                    or r + dr == ROWS
+                    or c + dc == COLS
+                    or (r + dr, c + dc) in visit
+                    or grid[r + dr][c + dc] == 1
+                ):
+                    continue
+                queue.append((r + dr, c + dc))
+                visit.add((r + dr, c + dc))
+        length += 1
